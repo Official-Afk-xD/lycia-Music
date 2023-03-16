f709b7140cf2f601200c2a4932e25c8ba9767d9d
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-16 23:06:44 +0530

    Update mongo.py

--- DevilxRobot/mongo.py
+++ DevilxRobot/mongo.py
@@ -3,20 +3,20 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from AsukaRobot import MONGO_DB_URI
-from AsukaRobot.confing import get_int_key, get_str_key
+from DevilxRobot import MONGO_DB_URI
+from DevilxRobot.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "AsukaRobot"
+MONGO_DB = "DevilxRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["AsukaRobot"]
+db = client["DevilxRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
