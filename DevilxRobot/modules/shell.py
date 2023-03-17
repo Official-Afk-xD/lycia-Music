39607ded80164d835efb9193f969bd3424ff324c
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 19:43:43 +0530

    Update shell.py

--- DevilxRobot/modules/shell.py
+++ DevilxRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from AsukaRobot import LOGGER, dispatcher
-from AsukaRobot.modules.helper_funcs.chat_status import dev_plus
+from DevilxRobot import LOGGER, dispatcher
+from DevilxRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
