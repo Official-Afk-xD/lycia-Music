669e075a865732c1d1751d946197638dff1b47fb
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 18:29:25 +0530

    Update locks.py

--- DevilxRobot/modules/locks.py
+++ DevilxRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import AsukaRobot.modules.sql.locks_sql as sql
-from AsukaRobot import dispatcher, DRAGONS, LOGGER
-from AsukaRobot.modules.disable import DisableAbleCommandHandler
-from AsukaRobot.modules.helper_funcs.chat_status import (
+import DevilxRobot.modules.sql.locks_sql as sql
+from DevilxRobot import dispatcher, DRAGONS, LOGGER
+from DevilxRobot.modules.disable import DisableAbleCommandHandler
+from DevilxRobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from AsukaRobot.modules.log_channel import loggable
-from AsukaRobot.modules.connection import connected
+from DevilxRobot.modules.log_channel import loggable
+from DevilxRobot.modules.connection import connected
 
-from AsukaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from DevilxRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
