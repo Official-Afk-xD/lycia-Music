25d370643021031523545145f0b345ae1c99ab9a
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 10:47:30 +0530

    Update global_bans.py

--- DevilxRobot/modules/global_bans.py
+++ DevilxRobot/modules/global_bans.py
@@ -14,9 +14,9 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-import AsukaRobot.modules.sql.global_bans_sql as sql
-from AsukaRobot.modules.sql.users_sql import get_user_com_chats
-from AsukaRobot import (
+import DevilxRobot.modules.sql.global_bans_sql as sql
+from DevilxRobot.modules.sql.users_sql import get_user_com_chats
+from DevilxRobot import (
     DEV_USERS,
     EVENT_LOGS,
     OWNER_ID,
@@ -30,16 +30,16 @@ from AsukaRobot import (
     sw,
     dispatcher,
 )
-from AsukaRobot.modules.helper_funcs.chat_status import (
+from DevilxRobot.modules.helper_funcs.chat_status import (
     is_user_admin,
     support_plus,
     user_admin,
 )
-from AsukaRobot.modules.helper_funcs.extraction import (
+from DevilxRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from AsukaRobot.modules.helper_funcs.misc import send_to_list
+from DevilxRobot.modules.helper_funcs.misc import send_to_list
 
 GBAN_ENFORCE_GROUP = 6
 
