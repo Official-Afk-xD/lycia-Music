5fa256b6c20990370ef4ea2d4e4919237d1654e1
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 10:42:25 +0530

    Update gettime.py

--- DevilxRobot/modules/gettime.py
+++ DevilxRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from AsukaRobot import TIME_API_KEY, dispatcher
-from AsukaRobot.modules.disable import DisableAbleCommandHandler
+from DevilxRobot import TIME_API_KEY, dispatcher
+from DevilxRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
