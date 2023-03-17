1146e95b29d23a5e3325f3935ae917cfdd967041
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 09:30:10 +0530

    Update and rename DevilxRobot/modules/blacklistusers.py to DevilxRobot/मॉड्यूल/blacklistusers.py

--- DevilxRobot/मॉड्यूल/blacklistusers.py
+++ DevilxRobot/मॉड्यूल/blacklistusers.py
@@ -1,12 +1,12 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import AsukaRobot.modules.sql.blacklistusers_sql as sql
-from AsukaRobot import (DEV_USERS, OWNER_ID, DRAGONS, DEMONS, TIGERS, WOLVES,
+import DevilxRobot.modules.sql.blacklistusers_sql as sql
+from DevilxRobot import (DEV_USERS, OWNER_ID, DRAGONS, DEMONS, TIGERS, WOLVES,
                           dispatcher)
-from AsukaRobot.modules.helper_funcs.chat_status import dev_plus
-from AsukaRobot.modules.helper_funcs.extraction import (extract_user,
+from DevilxRobot.modules.helper_funcs.chat_status import dev_plus
+from DevilxRobot.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from AsukaRobot.modules.log_channel import gloggable
+from DevilxRobot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
