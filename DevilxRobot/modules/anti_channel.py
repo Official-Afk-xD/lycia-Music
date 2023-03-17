2897316f44b0b15c81a62d19f70fadeae8586cff
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 09:12:16 +0530

    Update anti_channel.py

--- DevilxRobot/modules/anti_channel.py
+++ DevilxRobot/modules/anti_channel.py
@@ -1,5 +1,5 @@
 from telegram.ext.filters import Filters
-from AsukaRobot.modules.helper_funcs.decorators import Asukacmd, Asukamsg
+from DevilxRobot.modules.helper_funcs.decorators import Asukacmd, Asukamsg
 from telegram import Update, message
 from telegram.ext import CallbackContext
 from ..modules.helper_funcs.anonymous import user_admin, AdminPerms
@@ -7,7 +7,7 @@ import html
 from ..modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
 
-@Asukacmd(command="antichannel", group=100)
+@DevilxRobotcmd(command="antichannel", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -28,7 +28,7 @@ def set_antichannel(update: Update, context: CallbackContext):
         "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
-@Asukamsg(Filters.chat_type.groups, group=110)
+@DevilxRobotmsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
