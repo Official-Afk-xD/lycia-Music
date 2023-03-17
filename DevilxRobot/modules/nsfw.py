3ecbf065f0daf94d9e21f30995416111cdb1b34a
Տᴀᴜʀᴀʙʜ Տʜᴜᴋʟᴀ <97865824+Official-Afk-xD@users.noreply.github.com>
2023-03-17 18:49:27 +0530

    Update nsfw.py

--- DevilxRobot/modules/nsfw.py
+++ DevilxRobot/modules/nsfw.py
@@ -4,14 +4,14 @@ import nekos
 import requests
 from PIL import Image
 from telegram import ParseMode
-from AsukaRobot import dispatcher, updater
-import AsukaRobot.modules.sql.nsfw_sql as sql
-from AsukaRobot.modules.log_channel import gloggable
+from DevilxRobot import dispatcher, updater
+import DevilxRobot.modules.sql.nsfw_sql as sql
+from DevilxRobot.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
-from AsukaRobot.modules.helper_funcs.filters import CustomFilters
-from AsukaRobot.modules.helper_funcs.chat_status import user_admin
+from DevilxRobot.modules.helper_funcs.filters import CustomFilters
+from DevilxRobot.modules.helper_funcs.chat_status import user_admin
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 @run_async
